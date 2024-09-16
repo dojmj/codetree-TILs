7dff29426791d244cd6d94daa5f9037ef776a938
dojmj <132545176+dojmj@users.noreply.github.com>
2024-09-17 01:11:36 +0900

    Title: 살 수 있는 물건, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240917/살 수 있는 물건/things-able-to-buy.c
+++ 240917/살 수 있는 물건/things-able-to-buy.c
@@ -6,7 +6,7 @@ int main() {
     scanf("%d", &n);
     if(n >= 3000){
         printf("book");
-    } else if(1000 <= n < 3000){
+    } else if(n >= 1000){
         printf("mask");
     } else {
         printf("no");
