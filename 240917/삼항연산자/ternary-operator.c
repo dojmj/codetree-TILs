761d45cce5687d4a8bc9d8e68b0954ce66362383
dojmj <132545176+dojmj@users.noreply.github.com>
2024-09-17 00:56:01 +0900

    Title: 삼항연산자, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240917/삼항연산자/ternary-operator.c
+++ 240917/삼항연산자/ternary-operator.c
@@ -4,10 +4,6 @@ int main() {
     // 여기에 코드를 작성해주세요.
     int a;
     scanf("%d", &a);
-    if(a = 100) {
-        printf("pass");
-    } else {
-        printf("failure");
-    }
+    printf((a == 100) ? "pass" : "failure");
     return 0;
 }
